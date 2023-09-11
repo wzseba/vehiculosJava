c0e251f8df16945fcefd503479bb0e2a0637e986
wzseba <wzseba@gmail.com>
2023-09-10 23:01:27 -0300

    add: se agregaron atributos metodos abstract

--- src/uno/vehiculos/Vehiculo.java
+++ src/uno/vehiculos/Vehiculo.java
@@ -2,11 +2,31 @@ package uno.vehiculos;
 
 public abstract class Vehiculo {
 	
-	private String chofer;
-	private int kilometrosRecorridos;
+	/*Atributos*/
+	private Persona chofer;
+	private int kilometros;
 	
-	public Vehiculo(String chofer) {
-		this.chofer = chofer;
-		this.kilometrosRecorridos = 0;
+	/*Constructor*/
+	public Vehiculo() {
+		this.kilometros = 0;
 	}
+	
+	/*Metodos*/
+	public void asignarChofer(Persona nuevoChofer) {
+		this.chofer = nuevoChofer;
+	}
+	
+	public Persona getChofer() {
+		return this.chofer;
+	}
+
+	public int getKilometros() {
+		return this.kilometros;
+	}
+
+	public void setKilometros(int kilometros) {
+		this.kilometros = kilometros;
+	}
+	/*metodo que forzosamente tiene que estar incluido en las subclases*/
+	public abstract void cambiarChofer(Persona cbChofer);
 }
