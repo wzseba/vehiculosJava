53088fcfa99c2fa1c7ec9a646ee517d1f0efefff
wzseba <wzseba@gmail.com>
2023-09-10 23:02:15 -0300

    add: en proceso de construccion

--- src/uno/vehiculos/Autobus.java
+++ src/uno/vehiculos/Autobus.java
@@ -1,5 +1,22 @@
 package uno.vehiculos;
 
 public class Autobus extends Vehiculo {
+	
+	private Persona [] pasajeros;
+	private int cantidadAsientos = 0;
+	
+	public Autobus() {
+		super();
+	}
+	
+	public void subirPasajero(Persona pasajero) {
+		if(this.cantidadAsientos <= 10) {
+			pasajeros[cantidadAsientos] = pasajero;
+			cantidadAsientos++;
+		}
+	}
 
+	public void cambiarChofer(Persona cbChofer) {
+		
+	}
 }
