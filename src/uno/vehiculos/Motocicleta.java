84f7fe956d06fe85ac4ddc22ea8708cca1300091
wzseba <wzseba@gmail.com>
2023-09-10 22:17:39 -0300

    add: se agrego body de la clase Motocileta

--- src/uno/vehiculos/Motocicleta.java
+++ src/uno/vehiculos/Motocicleta.java
@@ -1,5 +1,23 @@
 package uno.vehiculos;
 
-public class Motocicleta {
+public class Motocicleta extends Vehiculo {
+	
+	/*Atributos*/
+	private Persona acompaniante;
+	
+	/*Constructor*/
+	public Motocicleta() {
+		super();
+	}
 
+	public void agregarAcompaniante(Persona acomp) {
+		this.acompaniante = acomp;
+	}
+
+	@Override
+	public void cambiarChofer(Persona cbChofer) {
+		if(this.acompaniante == null) {
+			super.asignarChofer(cbChofer);
+		}
+	}
 }
