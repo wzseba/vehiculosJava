8aebb759f4af45ab6d5245ff2dfeba225710e3b6
wzseba <wzseba@gmail.com>
2023-09-11 23:21:35 -0300

    fix: se arreglo error en el metodo subir pasajero

--- src/uno/vehiculos/Autobus.java
+++ src/uno/vehiculos/Autobus.java
@@ -1,22 +1,39 @@
 package uno.vehiculos;
 
 public class Autobus extends Vehiculo {
+	/*En caso del Autobús, no puede cambiar de chofer si hubiera pasajeros.*/
 	
-	private Persona [] pasajeros;
-	private int cantidadAsientos = 0;
+	private Persona [] pasajeros = new Persona[10];
+	int cantidadDePasajeros = 0;
 	
-	public Autobus() {
+	/*Constructor con cantidad de asientos*/
+	public Autobus(int cantidadDeAsientos) {
 		super();
+		this.pasajeros = new Persona[cantidadDeAsientos];
 	}
 	
-	public void subirPasajero(Persona pasajero) {
-		if(this.cantidadAsientos <= 10) {
-			pasajeros[cantidadAsientos] = pasajero;
-			cantidadAsientos++;
+	/*Constructor con argumentos de chofer y cantidad de asientos*/
+	public Autobus(Persona chofer, int cantidadAsientos) {
+		super(chofer);
+		this.pasajeros = new Persona[cantidadAsientos];
+	}
+	
+	public void subirPasajero(Persona pasajero) {//este metodo genera un error
+		if(this.cantidadDePasajeros < pasajeros.length) {
+			this.pasajeros[cantidadDePasajeros] = pasajero;
+			cantidadDePasajeros++;
+		}
+	}
+	
+	public void bajarPasajero() {
+		if(this.cantidadDePasajeros > 0) {
+			cantidadDePasajeros--;			
 		}
 	}
 
 	public void cambiarChofer(Persona cbChofer) {
-		
+		if(this.cantidadDePasajeros == 0) {
+			super.asignarChofer(cbChofer);
+		}
 	}
 }
